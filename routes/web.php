7e55011bfccbc14688fe126130b114b24bf44524
Sanjid121 <160921630+Sanjid121@users.noreply.github.com>
2025-02-28 16:54:04 +0600

    update

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/note', function () {
-    return response()->json(['message' => 'Welcome to Laravel API']);
+    return response()->json(['message' => 'Welcome ',
+'note'=> 'sanjid bhalo manus noi sha  kesu din dore bhalo babe kaj korcha naa',
+'id'=>1,
+
+
+]);
 
 
 });
